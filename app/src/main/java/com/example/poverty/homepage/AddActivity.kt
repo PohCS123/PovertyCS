13f3049c2c285ff6243499e403d4997feaaa86b7
Pohcs123 <gencapoh@gmail.com>
2020-01-05 17:58:05 +0800

    cscommit_1

--- app/src/main/java/com/example/poverty/homepage/AddActivity.kt
+++ app/src/main/java/com/example/poverty/homepage/AddActivity.kt
@@ -10,40 +10,5 @@ import kotlinx.android.synthetic.main.activity_add.*
 
 class AddActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add)
 
-        buttonSave.setOnClickListener {
-            saveUser()
-        }
-    }
-
-    private fun saveUser(){
-        /*if(TextUtils.isEmpty(editTextName.text)){
-            editTextName.setError(getString(R.string.error_value_required))
-            return
-        }
-        if(TextUtils.isEmpty(editTextContact.text)){
-            editTextContact.setError(getString(R.string.error_value_required))
-            return
-        }*/
-        val name = editTextName.text.toString()
-        val contact = editTextContact.text.toString()
-
-        val intent = Intent()
-        intent.putExtra(EXTRA_NAME, name)
-        intent.putExtra(EXTRA_CONTACT, contact)
-
-        setResult(Activity.RESULT_OK, intent)
-
-        finish()
-
-
-    }
-
-    companion object{
-        const val EXTRA_NAME = "my.edu.tarc.demo41.NAME"
-        const val EXTRA_CONTACT = "my.edu.tarc.demo41.CONTACT"
-    }
 }
